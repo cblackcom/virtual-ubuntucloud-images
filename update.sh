12d0a65da3b47fb6b3425cfb7f9513700bc6152e
Colin Black <cblack@cblack.com>
2020-11-30 01:44:10 +0000

    Re-add amd64 architecture to update loop

--- update.sh
+++ update.sh
@@ -2,7 +2,7 @@
 set -e
 cd "$( dirname "${BASH_SOURCE[0]}" )"
 
-for IAARCH in arm64; do
+for IAARCH in amd64 arm64; do
 	curl -o focal-$IAARCH.qcow2 https://cloud-images.ubuntu.com/focal/current/focal-server-cloudimg-$IAARCH.img
 	qemu-img resize focal-$IAARCH.qcow2 10G
 	qemu-img convert -f qcow2 focal-$IAARCH.qcow2 -O raw focal-$IAARCH.raw
